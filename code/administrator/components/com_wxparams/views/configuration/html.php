af046460cf374ed08f7f3b2a7e680f76114a9b8c
Arunas Mazeika <amazeika@wextend.com>
2011-04-07 11:44:04 +0200

    The XML file is now taken from the media/config folder of the calling component.
    Removed some no longer used CSS rules.

--- code/administrator/components/com_wxparams/views/configuration/html.php
+++ code/administrator/components/com_wxparams/views/configuration/html.php
@@ -6,20 +6,27 @@ class ComWxparamsViewConfigurationHtml extends ComWxparamsViewHtml {
 		
 		$toolbar = KFactory::get( 'admin::com.wxparams.toolbar.configuration' )->setTitle( WxText::_( 'WXPARAMS_CONFIGURATION' ) );
 		
-		$xml = new SimpleXMLElement( file_get_contents( dirname( __FILE__ ) . '/test.xml' ) );
-		
 		$model = $this->getModel();
 		$state = $model->getState();
 		
+		// Get the package name
 		if ($state->isUnique()) {
 			$row = $model->getItem();
 			$package = $row->package;
-			$form = WxparamsFactory::getForm( $xml, json_decode( $row->params ) );
 		} elseif ($state->package) {
 			$package = $state->package;
-			$form = WxparamsFactory::getForm( $xml );
 		} else {
 			throw new KViewException( 'Unable to determine the package name.' );
+		}
+		
+		$xml = new SimpleXMLElement( file_get_contents( JPATH_ROOT . '/media/' . $package . '/config/' . $package . '.xml' ) );
+		
+		if ($state->isUnique()) {
+			// Bind the parameters and render the form.
+			$form = WxparamsFactory::getForm( $xml, json_decode( $row->params ) );
+		} else {
+			// Just render the form.
+			$form = WxparamsFactory::getForm( $xml );
 		}
 		
 		$this->assign( 'package', $package );
