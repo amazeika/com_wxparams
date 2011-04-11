73c148ea33d58c0d8b27339bed1496bfeef92910
Arunas Mazeika <amazeika@wextend.com>
2011-04-11 11:26:50 +0200

    Added a unique index over the package and item_id columns. The validator now checks if a configuration for the provided package and item_id already exists before creating it.

--- code/administrator/components/com_wxparams/commands/validator.php
+++ code/administrator/components/com_wxparams/commands/validator.php
@@ -8,7 +8,18 @@ class ComWxparamsCommandValidator extends ComWextendCommandValidatorAbstract {
 	}
 	
 	protected function validateInput(KCommandContext $context, &$errors) {
-		// Nothing to do here.
+		
+		$data = $context->data;
+		
+		// Check if a configuration for the provided package and item_id already exists
+		$row = $context->caller->getModel()->getTable()->getRow();
+		
+		$row->setData( array ('package' => $data->package, 'item_id' => $data->item_id ) );
+		
+		if ($row->load() && KRequest::get( 'get.id', 'int' ) != $row->id) {
+			$errors [] = WxText::_( 'WXPARAMS_CONFIGURATION_EXISTS' );
+			return false;
+		}
 	}
 
 }
