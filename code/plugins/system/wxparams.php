ec52a248ab5e646b077bb8a1b98bce398ce0c82c
amazeika <amazeika@wextend.com>
2011-08-12 18:30:27 +0200

    Added loader plugin.

---
+++ code/plugins/system/wxparams.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @version     $Id: koowa.php 3678 2011-07-10 11:36:25Z johanjanssens $
+ * @category	Nooku
+ * @package     Nooku_Plugins
+ * @subpackage  System
+ * @copyright   Copyright (C) 2007 - 2010 Johan Janssens. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Koowa System plugin
+.*
+ * @author      Johan Janssens <johan@nooku.org>
+ * @category    Nooku
+ * @package     Nooku_Plugins
+ * @subpackage  System
+ */
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+class plgSystemWxparams extends JPlugin
+{
+	public function __construct($subject, $config = array())
+	{
+        // Load the WeXtend framework
+        require_once (JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_wxparams' . DS . 'includes' . DS . 'loader.php');
+	    
+		parent::__construct($subject, $config);
+	}
+}
