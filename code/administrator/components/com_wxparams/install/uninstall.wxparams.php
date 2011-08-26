a7d12e16300032c9866f8f566a2a14d6467d9a24
amazeika <amazeika@wextend.com>
2011-08-26 14:29:51 +0200

    Un-install any additional extension that was installed by the installer.

--- code/administrator/components/com_wxparams/install/uninstall.wxparams.php
+++ code/administrator/components/com_wxparams/install/uninstall.wxparams.php
@@ -1,1 +1,23 @@
 <?php
+defined('_JEXEC') or die('Restricted access');
+
+// Variable definitions
+$database = JFactory::getDBO();
+$source = $this->parent->getPath('source');
+$manifest = simplexml_load_file($this->parent->getPath('manifest'));
+$package = strtolower((string) $manifest->name);
+$jversion = JVersion::isCompatible('1.6.0') ? '1.6' : '1.5';
+
+//Run platform specific procedures
+require JPATH_ROOT . '/administrator/components/com_' . $package . '/install/uninstall.' . $jversion . '.php';
+
+// Delete framework folders
+foreach ($manifest->framework->folder as $folder)
+{
+    if(JFolder::exists(JPATH_ROOT.$folder)) JFolder::delete(JPATH_ROOT.$folder);
+}
+// Delete framework files
+foreach ($manifest->framework->file as $file)
+{
+    if(JFile::exists(JPATH_ROOT.$file)) JFile::delete(JPATH_ROOT.$file);
+}
