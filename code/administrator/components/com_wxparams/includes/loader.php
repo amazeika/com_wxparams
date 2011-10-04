c84174ebd9cf7046bfd6eace9d299623ecd9800c
amazeika <amazeika@wextend.com>
2011-10-04 11:18:44 +0200

    Added custom loader. Fixed loader script.

--- code/administrator/components/com_wxparams/includes/loader.php
+++ code/administrator/components/com_wxparams/includes/loader.php
@@ -12,21 +12,8 @@
 define('WXPARAMS_INCLUDES', dirname(__FILE__));
 define('WXPARAMS', 1);
 
-require_once (WXPARAMS_INCLUDES . DS . 'wxparams' . DS . 'factory.php');
-
-// SPL Autoload
-if(spl_autoload_functions() === false) {
-	if(function_exists('__autoload')) {
-		spl_autoload_register('__autoload', false);
-	}
-}
-
-// Checking if .php makes part of the current autoload extensions list adding it if necessary
-$autoload_extensions = spl_autoload_extensions();
-$autoload_extensions = str_replace(' ', '', $autoload_extensions);
-if(!in_array('.php', explode(',', $autoload_extensions))) {
-	spl_autoload_extensions($autoload_extensions . ',.php');
-}
-
-// Registering the custom autoload function
-spl_autoload_register(array('WxparamsFactory', 'autoload')); 
+require_once (WXPARAMS_INCLUDES . '/wxparams/loader/loader.php');
+$loader = new WxparamsLoader(array(
+	'class_prefix' => 'Wxparams', 
+	'lib_path' => WXPARAMS_INCLUDES . '/wxparams/'));
+$loader->setAutoload();
---
+++ code/administrator/components/com_wxparams/includes/wxparams/loader/loader.php
@@ -0,0 +1,5 @@
+<?php
+
+class WxparamsLoader extends WxLoader
+{
+}
