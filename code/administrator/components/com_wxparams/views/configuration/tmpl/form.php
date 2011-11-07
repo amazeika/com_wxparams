81e224b61fe0a18fee892f6f8f38ea9e710ac93d
amazeika <amazeika@wextend.com>
2011-11-07 12:54:42 +0100

    Removed WxHelperApplication call.

--- code/administrator/components/com_wxparams/views/configuration/tmpl/form.php
+++ code/administrator/components/com_wxparams/views/configuration/tmpl/form.php
@@ -31,8 +31,8 @@ wxjq(document).ready(function() {
 });
 </script>
 
-<form method="post" action="<?=@route('id=' . $configuration->id)?>" class="-koowa-form" id="mainform">
-<label for="title" class="mainlabel"><?=@text('WXPARAMS_TITLE');?></label>
+<form method="post" action="<?=@route('id=' . $configuration->id)?>"
+	class="-koowa-form" id="mainform"><label for="title" class="mainlabel"><?=@text('WXPARAMS_TITLE');?></label>
 <input id="title" type="text" name="title"
 	value="<?=@escape($configuration->title);?>" /><br />
 <label for="description" class="mainlabel"><?=@text('WXPARAMS_DESCRIPTION');?></label>
@@ -41,11 +41,12 @@ wxjq(document).ready(function() {
 if($form->getType() != 'global') {
 	?>
 <label for="item_id" class="mainlabel"><?=@text('WXPARAMS_MENU_ITEM');?></label>
-<?=@helper('com://admin/wxparams.template.helper.listbox.' . strtolower(WxHelperApplication::getName()) . '.menuitems');?><br />
+<?=@helper('com://admin/wxparams.template.helper.listbox.' . JVersion::isCompatible('1.6.0') ? 'j16' : 'j15' . '.menuitems');?><br />
 <?
 }
 ?>
 <? // Render the XML form
+
 echo $form->renderHtml();
 ?>
 <input type="hidden" name="package" value="<?=$form->getPackage();?>" />
