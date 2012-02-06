138b647af6356edd0779dc5c7de885db1f291929
amazeika <amazeika@wextend.com>
2012-02-06 10:53:15 +0100

    Pluralized the name of the dir containing config forms.

--- code/administrator/components/com_wxparams/factory.php
+++ code/administrator/components/com_wxparams/factory.php
@@ -25,22 +25,15 @@ class ComWxparamsFactory
 	 * Returns a form object.
 	 *
 	 * @param array $config Optional configuration array.
-	 * @return mixed ComWxparamsFormDefault or ComWxparamsFormTabbed depending on the XML form.
+	 * @return WxForm The form object.
 	 */
 	public static function getForm($config = array())
 	{
 		$config = new KConfig($config);
 		
-		$xml = new SimpleXMLElement(file_get_contents(JPATH_ROOT . '/media/' . $config->package . '/config/' . str_replace('.', '/', $config->type) . '/form.xml'));
-		
-		$config = $config->toArray();
-		foreach($xml->children() as $name => $element) {
-			// A form is considered as tabbed if every root element is a tab element.
-			if($name != 'tab') {
-				return KService::get('com://admin/wxparams.form.default', $config)->importXml($xml);
-			}
-		}
-		return KService::get('com://admin/wxparams.form.tabbed', $config)->importXml($xml);
+		$form = WxHelperApplication::getPath('admin') . '/components/' . $config->package . '/configs/' . str_replace('.', '/', $config->type) . '/form.xml';
+
+		return KService::get('wextend:form', array('form' => $form, 'data' => $config->params, 'container' => 'params'));
 	}
 	
 	/**
