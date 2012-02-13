9b052503fd86b96c914a7e06e83faf14dfca8a26
amazeika <amazeika@wextend.com>
2012-02-13 22:46:37 +0100

    Changed SQL filenames.
    Added indexes.

--- code/administrator/components/com_wxparams/install/install.sql
+++ code/administrator/components/com_wxparams/install/install.sql
@@ -1,7 +1,3 @@
---
--- Structure de la table `#__wxparams_configurations`
---
-
 CREATE TABLE IF NOT EXISTS `#__wxparams_configurations` (
   `wxparams_configuration_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
@@ -13,5 +9,7 @@ CREATE TABLE IF NOT EXISTS `#__wxparams_configurations` (
   `default` tinyint(1) NOT NULL,
   `locked_by` int(11) NOT NULL DEFAULT '0',
   `locked_on` datetime NOT NULL,
-  UNIQUE KEY `wxparams_configuration_id` (`wxparams_configuration_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`wxparams_configuration_id`),
+  KEY `idx-default` (`default`),
+  KEY `idx-package_type` (`package`,`type`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
